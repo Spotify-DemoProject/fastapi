f1aec67a10c1056e7a4c1235d7d88edd1d3bac1f
hooniegit <neivekim76@gmail.com>
2023-12-28 20:03:57 +0900

    231228 fix: 라우터 포워딩 수정

--- router/parquet_routers.py
+++ router/parquet_routers.py
@@ -1,16 +1,16 @@
 from fastapi import APIRouter
-from util.kafka_utils import *
+from util.parquet_utils import *
 
 router = APIRouter()
 
 @router.get("/parquet/albums")
 async def get_albums(insert_date:str):
-	return(publish_message_kafka(insert_date=insert_date, category="albums"))
+	return(upload_parquets(insert_date=insert_date, category="albums"))
 
 @router.get("/parquet/artists")
 async def get_artists(insert_date:str):
-	return(publish_message_kafka(insert_date=insert_date, category="artists"))
+	return(upload_parquets(insert_date=insert_date, category="artists"))
 
 @router.get("/parquet/tracks")
 async def get_artists(insert_date:str):
-	return(publish_message_kafka(insert_date=insert_date, category="tracks"))
+	return(upload_parquets(insert_date=insert_date, category="tracks"))
